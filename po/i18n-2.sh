3002bc61a0a0c114bc946d2a4f108172e29b5d3a
Muflone <muflone@vbsimple.net>
2010-02-13 15:11:24 +0000

    Preparing for release 0.4.1
    git-svn-id: http://gwakeonlan.googlecode.com/svn/trunk@35 fc2f1c62-dc75-11de-9d50-21f6430a363f

--- po/i18n-2.sh
+++ po/i18n-2.sh
@@ -3,4 +3,20 @@ msginit --input=gwakeonlan.pot --output-file=en_US.po --locale=en_US
 msginit --input=gwakeonlan.pot --output-file=it.po --locale=it_IT
 msginit --input=gwakeonlan.pot --output-file=fr.po --locale=fr_FR
 msginit --input=gwakeonlan.pot --output-file=es.po --locale=es_ES
-
+msginit --input=gwakeonlan.pot --output-file=ru.po --locale=ru_RU
+read
+msginit --input=gwakeonlan.pot --output-file=ar.po --locale=ar
+msginit --input=gwakeonlan.pot --output-file=bg.po --locale=bg
+msginit --input=gwakeonlan.pot --output-file=cs.po --locale=cs
+msginit --input=gwakeonlan.pot --output-file=da.po --locale=da
+msginit --input=gwakeonlan.pot --output-file=de.po --locale=de_DE
+msginit --input=gwakeonlan.pot --output-file=he.po --locale=he
+msginit --input=gwakeonlan.pot --output-file=hu.po --locale=hu
+msginit --input=gwakeonlan.pot --output-file=ja.po --locale=ja
+msginit --input=gwakeonlan.pot --output-file=nl.po --locale=nl_NL
+msginit --input=gwakeonlan.pot --output-file=pl.po --locale=pl
+msginit --input=gwakeonlan.pot --output-file=pt.po --locale=pt_PT
+msginit --input=gwakeonlan.pot --output-file=sk.po --locale=sk
+msginit --input=gwakeonlan.pot --output-file=tr.po --locale=tr
+msginit --input=gwakeonlan.pot --output-file=zh.po --locale=zh_CH
+read
--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class InstallData(install_data):
 
 
 setup(name='gWakeOnLan',
-      version='0.4',
+      version='0.4.1',
       description='Wake up your machines using Wake on LAN.',
       author='Fabio Castelli',
       author_email='muflone@vbsimple.net',
