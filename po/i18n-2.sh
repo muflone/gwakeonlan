dad6a9339faa6a7dc215e674bebb78c84e2b0d98
Muflone <muflone@vbsimple.net>
2010-02-19 14:31:31 +0000

    fixed russian and french translation
    git-svn-id: http://gwakeonlan.googlecode.com/svn/trunk@53 fc2f1c62-dc75-11de-9d50-21f6430a363f

--- po/i18n-2.sh
+++ po/i18n-2.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-msginit --input=gwakeonlan.pot --output-file=en_US.po --locale=en_US
-msginit --input=gwakeonlan.pot --output-file=it.po --locale=it_IT
-msginit --input=gwakeonlan.pot --output-file=fr.po --locale=fr_FR
-msginit --input=gwakeonlan.pot --output-file=es.po --locale=es_ES
-msginit --input=gwakeonlan.pot --output-file=ru.po --locale=ru_RU
-msginit --input=gwakeonlan.pot --output-file=ar.po --locale=ar
-msginit --input=gwakeonlan.pot --output-file=bg.po --locale=bg
-msginit --input=gwakeonlan.pot --output-file=cs.po --locale=cs
-msginit --input=gwakeonlan.pot --output-file=da.po --locale=da
-msginit --input=gwakeonlan.pot --output-file=de.po --locale=de_DE
-msginit --input=gwakeonlan.pot --output-file=he.po --locale=he
-msginit --input=gwakeonlan.pot --output-file=hu.po --locale=hu
-msginit --input=gwakeonlan.pot --output-file=ja.po --locale=ja
-msginit --input=gwakeonlan.pot --output-file=nl.po --locale=nl_NL
-msginit --input=gwakeonlan.pot --output-file=pl.po --locale=pl
-msginit --input=gwakeonlan.pot --output-file=pt.po --locale=pt_PT
-msginit --input=gwakeonlan.pot --output-file=sk.po --locale=sk
-msginit --input=gwakeonlan.pot --output-file=tr.po --locale=tr
-msginit --input=gwakeonlan.pot --output-file=zh_CN.po --locale=zh_CN
+msginit --no-translator --input=gwakeonlan.pot --output-file=en_US.po --locale=en_US
+msginit --no-translator --input=gwakeonlan.pot --output-file=it.po --locale=it_IT
+msginit --no-translator --input=gwakeonlan.pot --output-file=fr.po --locale=fr_FR
+msginit --no-translator --input=gwakeonlan.pot --output-file=es.po --locale=es_ES
+msginit --no-translator --input=gwakeonlan.pot --output-file=ru.po --locale=ru_RU
+msginit --no-translator --input=gwakeonlan.pot --output-file=ar.po --locale=ar
+msginit --no-translator --input=gwakeonlan.pot --output-file=bg.po --locale=bg
+msginit --no-translator --input=gwakeonlan.pot --output-file=cs.po --locale=cs
+msginit --no-translator --input=gwakeonlan.pot --output-file=da.po --locale=da
+msginit --no-translator --input=gwakeonlan.pot --output-file=de.po --locale=de_DE
+msginit --no-translator --input=gwakeonlan.pot --output-file=he.po --locale=he
+msginit --no-translator --input=gwakeonlan.pot --output-file=hu.po --locale=hu
+msginit --no-translator --input=gwakeonlan.pot --output-file=ja.po --locale=ja
+msginit --no-translator --input=gwakeonlan.pot --output-file=nl.po --locale=nl_NL
+msginit --no-translator --input=gwakeonlan.pot --output-file=pl.po --locale=pl
+msginit --no-translator --input=gwakeonlan.pot --output-file=pt.po --locale=pt_PT
+msginit --no-translator --input=gwakeonlan.pot --output-file=sk.po --locale=sk
+msginit --no-translator --input=gwakeonlan.pot --output-file=tr.po --locale=tr
+msginit --no-translator --input=gwakeonlan.pot --output-file=zh_CN.po --locale=zh_CN
+echo ok
 read
