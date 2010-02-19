d62ec454fb5f870cad86fc5f3c430c2b4a6437c4
Muflone <muflone@vbsimple.net>
2010-02-19 10:38:52 +0000

    fixed version number from 0.4.1 to 0.5
    git-svn-id: http://gwakeonlan.googlecode.com/svn/trunk@52 fc2f1c62-dc75-11de-9d50-21f6430a363f

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class InstallData(install_data):
 
 
 setup(name='gWakeOnLan',
-      version='0.4.1',
+      version='0.5',
       description='Wake up your machines using Wake on LAN.',
       author='Fabio Castelli',
       author_email='muflone@vbsimple.net',
