1fca45b2769c7c9092c523e9d4637a5c7cacd4b3
Muflone <webreg@vbsimple.net>
2013-10-06 18:35:03 +0200

    Bumped version number to 0.5.2

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class InstallData(install_data):
 
 
 setup(name='gWakeOnLan',
-      version='0.5.1',
+      version='0.5.2',
       description='Wake up your machines using Wake on LAN.',
       author='Fabio Castelli',
       author_email='muflone@vbsimple.net',
