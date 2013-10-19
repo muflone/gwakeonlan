4771204b3681eb10cf20ef19489fcd22d33c781f
Muflone <webreg@vbsimple.net>
2013-10-19 19:11:04 +0200

    Updated setup.py with translations support

--- setup.py
+++ setup.py
@@ -22,60 +22,82 @@
 from distutils.core import setup
 from distutils.command.install_scripts import install_scripts
 from distutils.command.install_data import install_data
+from distutils.log import info
 
 import os
 import os.path
 import shutil
+from itertools import chain
 from glob import glob
 from gwakeonlan.constants import *
 
-class rename_python_scripts(install_scripts):
-  "Rename main executable python script without .py extension"
+class Install_Scripts(install_scripts):
   def run(self):
     install_scripts.run(self)
+    self.rename_python_scripts()
+
+  def rename_python_scripts(self):
+    "Rename main executable python script without .py extension"
     for script in self.get_outputs():
       if script.endswith(".py"):
+        info('renaming the python script %s -> %s' % (script, script[:-3]))
         shutil.move(script, script[:-3])
 
-class install_icons(install_data):
-  "Install icons in the hicolor theme directory"
+class Install_Data(install_data):
   def run (self):
+    self.install_icons()
+    self.install_translations()
+    install_data.run(self)
+  
+  def install_icons(self):
+    info('Installing icons...')
     DIR_ICONS = 'icons'
     for icon_format in os.listdir(DIR_ICONS):
       icon_dir = os.path.join(DIR_ICONS, icon_format)
       self.data_files.append((
         os.path.join('share', 'icons', 'hicolor', icon_format, 'apps'),
         glob(os.path.join(icon_dir, '*'))))
-    install_data.run(self)
+
+  def install_translations(self):
+    info('Installing translations...')
+    for po in glob(os.path.join('po', '*.po')):
+      lang = os.path.basename(po[:-3])
+      mo = os.path.join('build', 'mo', lang, '%s.mo' % DOMAIN_NAME)
+
+      directory = os.path.dirname(mo)
+      if not os.path.exists(directory):
+        info('creating %s' % directory)
+        os.makedirs(directory)
+
+      cmd = 'msgfmt -o %s %s' % (mo, po)
+      info('compiling %s -> %s' % (po, mo))
+      if os.system(cmd) != 0:
+        raise SystemExit('Error while running msgfmt')
+
+      dest = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
+      self.data_files.append((dest, [mo]))
 
 setup(
   name=APP_NAME,
   version=APP_VERSION,
-  description=APP_DESCRIPTION,
   author=APP_AUTHOR,
   author_email=APP_AUTHOR_EMAIL,
   maintainer=APP_AUTHOR,
   maintainer_email=APP_AUTHOR_EMAIL,
   url=APP_URL,
+  description=APP_DESCRIPTION,
   license='GPL v2',
   scripts=['gwakeonlan.py'],
   packages=['gwakeonlan'],
   data_files=[
-    ('share/gwakeonlan/data', (
-      'data/gwakeonlan.png',
-    )),
-    ('share/gwakeonlan/ui',
-      glob('ui/*')
-    ),
-    ('share/doc/gwakeonlan',
-      glob('doc/*')
-    ),
-    ('share/applications', (
-      'data/gwakeonlan.desktop',
-    ))
+    ('share/gwakeonlan/data', ['data/gwakeonlan.png']),
+    ('share/applications', ['data/gwakeonlan.desktop']),
+    ('share/doc/gwakeonlan', glob('doc/*')),
+    ('share/man/man1', ['man/gwakeonlan.1']),
+    ('share/gwakeonlan/ui', glob('ui/*')),
   ],
   cmdclass = {
-    'install_scripts': rename_python_scripts,
-    'install_data': install_icons
+    'install_scripts': Install_Scripts,
+    'install_data': Install_Data
   }
 )
