2a6607e45b73a18e0676ccfe2b41b553c9498b42
Muflone <webreg@vbsimple.net>
2015-07-18 19:29:12 +0200

    Whitespaces fixes

--- create-pot.sh
+++ create-pot.sh
@@ -3,16 +3,16 @@ POTFILE="po/gwakeonlan.pot.new"
 
 if [ -f ui/*.glade.h ]
 then
-  rm ui/*.glade.h
+    rm ui/*.glade.h
 fi
 for _gladefile in ui/*.glade
 do
-  intltool-extract --type=gettext/glade ${_gladefile}
+    intltool-extract --type=gettext/glade ${_gladefile}
 done
 
 if ! [ -f $POTFILE ]
 then
-  touch $POTFILE
+    touch $POTFILE
 fi
 xgettext --language=Python --keyword=_ --keyword=N_ --output $POTFILE ui/*.glade.h *.py gwakeonlan/*.py
 rm ui/*.glade.h
