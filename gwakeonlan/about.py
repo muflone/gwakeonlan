1ec8f4a4e2dd274d5f7eec48de2d873fbf7c4bd0
Muflone <webreg@vbsimple.net>
2015-07-26 19:39:53 +0200

    Added fix for add_credit_section for GTK 3.6+

--- gwakeonlan/about.py
+++ gwakeonlan/about.py
@@ -59,9 +59,11 @@ class AboutWindow(object):
         self.dialog.set_license('\n'.join(readlines(FILE_LICENSE, True)))
         self.dialog.set_translator_credits('\n'.join(translators))
         # Retrieve the external resources links
-        for line in readlines(FILE_RESOURCES, False):
-            resource_type, resource_url = line.split(':', 1)
-            self.dialog.add_credit_section(resource_type, (resource_url,))
+        # only for GTK+ 3.6.0 and higher
+        if Gtk.check_version(3, 6, 0):
+            for line in readlines(FILE_RESOURCES, False):
+                resource_type, resource_url = line.split(':', 1)
+                self.dialog.add_credit_section(resource_type, (resource_url,))
         icon_logo = Pixbuf.new_from_file(FILE_ICON)
         self.dialog.set_logo(icon_logo)
         self.dialog.set_transient_for(winParent)
